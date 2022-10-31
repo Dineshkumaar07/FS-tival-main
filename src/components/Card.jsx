c2c4c59ede2f45584cb51f4ff7bd067b1a709c48
Dineshkumaar07 <dineshkumaar2003@gmail.com>
2022-11-01 00:09:19 +0530

    Update Carosuel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,10 +10,7 @@ function Card() {
 
                     <img className="w-[400px] inline-block p-2 cursor-pointer hover:scale-105 ease-in duration-300" src={item.img} alt="/" />
                 ))}
-                <br />
-                {data.map((item) => (
-                    <p className="w-[400px] text-center inline-block p-2 cursor-pointer ">{item.abt}</p>
-                ))}
+
             </div>
         </>
     )
